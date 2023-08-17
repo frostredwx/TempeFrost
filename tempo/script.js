a4638d06a19d20cc8e978bfca8d50fcde68463b9
frostredwx <139906525+frostredwx@users.noreply.github.com>
2023-08-17 17:01:48 -0300

    frost

---
+++ tempo/script.js
@@ -0,0 +1,25 @@
+
+
+const key = "838d4c590a88aa76e16b6e135b26f191"
+
+function colocardadosnatela(dados) {
+    console.log(dados)
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".texto-previsao").innerHTML = dados.weather[0].description
+    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
+    document.querySelector(".img-previsao").src = `https://openweathermap.org/img/wn/${dados.weather[0].icon}.png`
+}
+
+async function buscarCidade(cidade) {
+
+    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(Response => Response.json())
+
+    colocardadosnatela(dados)
+}
+
+function cliqueinobotao() {
+    const cidade = document.querySelector(".input-cidade").value
+
+    buscarCidade(cidade)
+}
